9e5fd18dae8eb5e35ef6309f2ae8fcca889e8f43
Anderson Contreira <anderson.contreira@gmail.com>
2021-01-31 16:48:26 -0300

    ajuste de bin para ecr

--- bin/aws/ecr-init.sh
+++ bin/aws/ecr-init.sh
@@ -1,3 +1,3 @@
 echo "AWS Account: $1"
-echo "aws ecr get-login-password --region sa-east-1 --profile tcc-td-puc-minas-admin"
-aws ecr get-login-password --region sa-east-1 --profile tcc-td-puc-minas-admin
+echo "aws ecr get-login-password --region sa-east-1 --profile tcc-td-puc-minas-admin --no-include-email"
+aws ecr get-login-password --region sa-east-1 --profile tcc-td-puc-minas-admin --no-include-email
