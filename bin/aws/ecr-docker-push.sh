ba8e1d377ba4ef2ff61b2d4a63837aece0c6cb3e
Anderson Contreira <anderson.contreira@gmail.com>
2021-04-08 21:45:02 -0300

    removida a pasta bin

--- bin/aws/ecr-docker-push.sh
+++
@@ -1,3 +0,0 @@
-echo "AWS Account: $1"
-echo "docker push $1.dkr.ecr.sa-east-1.amazonaws.com/sigo-frontend:latest"
-docker push $1.dkr.ecr.sa-east-1.amazonaws.com/sigo-frontend:latest
--- bin/aws/ecr-docker-tag.sh
+++
@@ -1,3 +0,0 @@
-echo "AWS Account: $1"
-echo "docker tag sigo-frontend:latest $1.dkr.ecr.sa-east-1.amazonaws.com/sigo-frontend:latest"
-docker tag sigo-frontend:latest $1.dkr.ecr.sa-east-1.amazonaws.com/sigo-frontend:latest
